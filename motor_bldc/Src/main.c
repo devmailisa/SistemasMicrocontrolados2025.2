0a55392d0a739476f46d2e7cbc8363d832bd8171
Isabelle Medeiros <89810072+devmailisa@users.noreply.github.com>
2025-11-12 23:10:38 -0300

    Implement throttle control based on UART input
    Added throttle function to generate pulse based on UART input.

--- motor_bldc/Src/main.c
+++ motor_bldc/Src/main.c
@@ -7,11 +7,35 @@
 #define baudRate 	115200
 #define clk			16000000
 
+#define dCMin 53
+#define dCMax 118
+
+
+//Gerando pulso de throller a partir do valor de entrada na uart2
+void thr(uint32_t en, double dCAtual)
+{
+	uint8_t dc;
+	double passo = (dCMax - dCMin)/100;
+	dc = 53 + passo*en;
+
+	//duty cycle de 53 a 118
+	while(1)
+	{
+		//Configurar a rampa aqui
+	}
+}
+
+
 int main(void)
 {
 	configurarTimer2();
+	fpuEnable();
+	uart2RxTxIni(baudRate, clk);
 
-	while(1){
+	while(1)
+	{
+		//O SINAL ESTÁ SAINDO NA PA0 QUE JÁ ESTÁ CONFIGURADA
+		//A UART RECEBE PELO PA2 E ENVIA PELO PA3
 
 	}
 }
