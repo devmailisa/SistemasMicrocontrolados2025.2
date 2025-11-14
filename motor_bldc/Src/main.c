739042e878e953744769541607e5192e65a7c1a3
Isabelle Medeiros <89810072+devmailisa@users.noreply.github.com>
2025-11-14 20:26:07 -0300

    Implementação do throller e do tratamento da entrada

--- motor_bldc/Src/main.c
+++ motor_bldc/Src/main.c
@@ -1,9 +1,18 @@
+/*
+ * CONTROLADOR DO MOTOR BLDC COM MCU STM32F446RExx
+ */
+
 #include <stdint.h>
-#include <systick.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include <systick.h>
 #include <fpu.h>
 #include <uart2.h>
 #include <timer2.h>
+
 #define baudRate 	115200
 #define clk			16000000
 
@@ -12,54 +21,81 @@
 
 
 //Gerando pulso de throller a partir do valor de entrada na uart2
-int thr(uint32_t en, uint32_t dcAtual)
+int thr(uint32_t en, uint32_t *dcAtual)
 {
-	if(en < 1 || en > 100) return 0;
+    if(en < 1 || en > 100) return 0;
 
-	uint32_t dc;
-	float passo;
+    uint32_t dc;
+    float passo;
+    passo = (dCMax - dCMin)/99.0;
 
-	passo = (dCMax - dCMin)/99;
-	dc = dCMin + passo*(en - 1); //COMPARADOR
+    //printf("passo: %f\r\n", passo);
+    dc = dCMin + passo*(en - 1); //COMPARADOR
+    //printf("dc: %d\r\n", dc);
 
-	if(dcAtual > dc)
-	{
-		while(dcAtual != dc)
-		{
-			uint32_t diff = dcAtual - dc;
-			if(diff >= 2)
-			{
-				dcAtual -= 2;
-			} else
-			{
-				dcAtual -= diff;
-			}
+    if(*dcAtual > dc)
+    {
+        while(*dcAtual != dc)
+        {
+            uint32_t diff = *dcAtual - dc;
+            if(diff >= 2)
+            {
+                *dcAtual -= 2;
+            } else
+            {
+                *dcAtual -= diff;
+            }
+        }
 
-			setDuty(1, dcAtual);
-			delayMs(50);
-		}
+    } else if(*dcAtual < dc)
+    {
+        while(*dcAtual != dc)
+        {
+            uint32_t diff = dc - *dcAtual;
+            if(diff >= 2)
+            {
+                *dcAtual += 2;
+            } else
+            {
+                *dcAtual += diff;
+            }
+        }
+    }
 
-	} else if(dCAtual < dc)
-	{
-		while(dcAtual != dc)
-		{
-			uint32_t diff = dc - dcAtual;
-			if(diff >= 2)
-			{
-				dcAtual += 2;
-			} else
-			{
-				dcAtual += diff;
-			}
+    //printf("dcAtual: %d\r\n", *dcAtual);
 
-			setDuty(1, dcAtual);
-			delayMs(50);
-		}
-	}
-
-	return dcAtual;
+    return 1;
 }
 
+
+int check_in(char * entrada, char * func, uint8_t * val) {
+
+    int final_func = strnlen(func, 20);
+
+    if (strncmp(entrada, func, final_func) != 0) {
+        return 0;
+    }
+
+    int digitos = 0;
+    int i = final_func;
+    char num[3];
+    while (isdigit(entrada[i])) {
+        num[digitos] = entrada[i];
+        digitos++;
+        i++;
+        if (digitos > 3) {
+            return 0;
+        }
+    }
+
+    if (entrada[i] != '\0') {
+        return 0;
+    }
+
+    *val = atoi(num);
+    //printf("%d\r\n", *val);
+    return 1;
+}
 
 int main(void)
 {
@@ -67,10 +103,31 @@ int main(void)
 	fpuEnable();
 	uart2RxTxIni(baudRate, clk);
 
+	char * entrada;
+	uint8_t val;
+	uint32_t dcAtual = 53;
+
 	while(1)
 	{
-		//O SINAL ESTÁ SAINDO NA PA0 QUE JÁ ESTÁ CONFIGURADA
-		//A UART RECEBE PELO PA2 E ENVIA PELO PA3
+		entrada = uart2ReadString();
 
+		if(check_in(entrada, "THR ", &val))
+		{
+			//chama THR
+			int err_thr = thr(val, &dcAtual);
+			if(!err_thr)
+			{
+				printf("ERR:ALVO DO THROLLER FORA DO INTERVALO DE [0, 100]\r\n");
+			} else
+			{
+				printf("THROLLER AJUSTADO COM SUCESSO\r\n");
+			}
+		} else if (check_in(entrada, "SET STEP=", &val))
+		{
+			//chama SET STEP
+		} else if (check_in(entrada, "SET HOLD=", &val))
+		{
+			//chama SET HOLD
+		}
 	}
 }
